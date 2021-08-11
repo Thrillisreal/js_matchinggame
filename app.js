472e17d70d773a2b3abdb8fffea891d027a81f06
Micah <micahstewart324@gmail.com>
2021-08-11 00:53:53 -0400

    adding images

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 let card = document.querySelectorAll('.card-inside')
+// let card2 = 
+
+
+// function start(){
+  
+// }
+function showCard(){
+ console.log('The card is clicked')
+} 
+console.log(showCard())
+let clickedCard = document.querySelectorAll('.card-inside').addEventListener('click', showCard())
+//Check if innerText works to append it to the DOM//
+function match(){
+  if(card === card2){
+    document.innerText = 'You got a match!'
+    console.log('This function works');
+  }else{
+    document.innerText = 'Try again!'
+  }
+}
+
