8726d6006f98f9df4ee82d95e8bf3f51bb23c1dd
Micah <micahstewart324@gmail.com>
2021-08-12 23:14:04 -0400

    JS Finished!

--- app.js
+++ app.js
@@ -1,81 +1,88 @@
+const outsideCards = document.querySelectorAll('.card-outside')
+const shuffledOutsideCards = shuffle(outsideCards)
+const board = document.querySelector('.board')
+const message = document.querySelector('#message')
+
+
+
+let numbMatchedCards = 0
+
 let cards = document.querySelectorAll('.card-inside')
-let button = document.getElementById('')
-
-// let card2 = 
 let turn = 1
-
-
 let currentCards = []
 
-function flip(card){
- //get source of the first card//
-card.lastElementChild.lastElementChild.src=card.firstElementChild.firstElementChild.src
- console.log(card.firstElementChild.firstElementChild);
+shuffle(cards)
+function flip(card) {
+  //get source of the first card//
+  console.log('card', card)
+  card.lastElementChild.lastElementChild.src =
+    card.firstElementChild.firstElementChild.src
+  console.log(card.firstElementChild.firstElementChild)
 }
 
-function flipBack(card){
+function flipBack(card) {
   //set image source back to the back of the card
-  card.src='back.png'
-//e.target when calling//
+  card.src = 'back.png'
 }
-cards.forEach(card => {
-  card.addEventListener('click', function(e){
-    if(turn === 1){
+cards.forEach((card) => {
+  card.addEventListener('click', function (e) {
+    if (turn === 1) {
       // A function that flips the card//
       flip(card)
       currentCards.push(e.target)
-      
+
       turn = 2
-    }else {
+    } else {
       flip(card)
       // Pushes card into the array //
       currentCards.push(e.target)
-  //Check if sources are the same, and if they are then keep then back with conditional
-      
-  
-  
-  // flipBack(e.target)
-      // Checks to see if theres a match //
-      
-      // Empty of cards from the array we created //
-      currentCards = []
-      
-      //If there is a match, the cards stays flipped, and if not it flips back over//
-      
-      
-      //A function that flips the card the second turn //
+      //Check if sources are the same, and if they are then keep then back with conditional
+      if (currentCards[0].src === currentCards[1].src) {
+        // check if sources match//
+        console.log('match')
+        numbMatchedCards++
+        currentCards = []
+        checkWin()
+      } else {
+        // flips them back //
+        console.log('no match')
+        console.log(currentCards)
+        setTimeout(function () {
+          flipBack(currentCards[0])
+          flipBack(currentCards[1])
+          currentCards = []
+        }, 1500)
+      }
       turn = 1
     }
-  console.log(turn, currentCards)
   })
-
 })
 
 
-// function stop(){
-  
-// }
+function shuffle(cards){
+  // not my code, recieved from James Bubb online
+  //https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
+const cardsArray = [...cards]
 
 
-
-
-// function showCard(){
-//  console.log('The card is clicked')
-// } 
-// console.log(showCard())
-// let clickedCard = document.querySelectorAll('.card-inside').addEventListener('click', showCard())
-//Check if innerText works to append it to the DOM//
-
-
-
-
-
-function match(){
-  if(card === card2){
-    document.innerText = 'You got a match!'
-    console.log('This function works');
-  }else{
-    document.innerText = 'Try again!'
-  }
+  const shuffledCards = cardsArray.sort((a, b) => 0.5 - Math.random());
+  shuffledCards.forEach(currentCard => {
+ console.log(currentCard.firstElementChild.firstElementChild);
+  })
+  return shuffledCards
 }
 
+function showNewCards(){
+ board.innerHTML = ''
+ shuffledOutsideCards.forEach(currentCard => {
+   board.append(currentCard)
+  // console.log(currentCard.firstElementChild.firstElementChild);
+   })
+}
+showNewCards()
+
+
+function checkWin(){
+  if(numbMatchedCards === 8)
+  message.innerHTML = 'You Win This Time...'
+}
